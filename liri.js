a07ab977e034bdfbc6fb77e5fdec73ec13e128cb
Claude Meurehg <claude.meurehg@mail.utoronto.ca>
2018-06-22 14:22:24 -0400

    further steps completed, need to take process.argv inputs now

--- liri.js
+++ liri.js
@@ -1,3 +1,12 @@
 require("dotenv").config();
 var keys = require ("./keys.js");
-console.log(keys);
+
+var r = require ("request");
+var urlHit = "http://www.omdbapi.com/?t=Jaws&y=&plot=full&tomatoes=true&apikey=trilogy";
+
+r (urlHit, function (error, response, body){
+    var arrange = JSON.parse(body);
+    console.log (arrange.Title);
+    console.log (arrange.Year);
+    console.log (arrange.Plot);
+});
